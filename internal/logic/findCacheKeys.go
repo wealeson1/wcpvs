1bbc860c32074b0fbb8b94a5606a2b3fee3d8c18
wealeson1 <goudanwang81@gmail.com>
2024-07-23 17:41:59 +0800

    Optimize the detection logic for request header cache key validation

--- internal/logic/findCacheKeys.go
+++ internal/logic/findCacheKeys.go
@@ -1,7 +1,6 @@
 package logic
 
 import (
-	"fmt"
 	"github.com/projectdiscovery/gologger"
 	"github.com/wealeson1/wcpvs/internal/logic/tecniques"
 	"github.com/wealeson1/wcpvs/internal/models"
@@ -51,10 +50,8 @@ func (f *FindCacheKeys) Check(target *models.TargetStruct) error {
 		}
 	}
 
-	if ih, headers := f.BinarySearchHeaders(target); ih && len(headers) != 0 {
-		gologger.Info().Msgf("The target %s has request header cache key(s): %v.", target.Request.URL, headers)
-		target.Cache.CKIsHeader = true
-		target.Cache.HeaderCacheKeys = headers
+	if f.FindCacheKeyByHeader(target) {
+		gologger.Info().Msgf("The target %s has request header cache key(s): %v.", target.Request.URL, target.Cache.HeaderCacheKeys)
 	}
 
 	if target.Cache.CKIsAnyGet || target.Cache.CKIsHeader {
@@ -84,7 +81,7 @@ func (f *FindCacheKeys) Check(target *models.TargetStruct) error {
 	}
 
 	if target.Cache.CKIsAnyGet || target.Cache.CKIsHeader || target.Cache.CKisCookie || target.Cache.CKIsGet {
-		target.Cache.NoCache = true
+		target.Cache.NoCache = false
 	}
 
 	return nil
@@ -121,157 +118,131 @@ func (f *FindCacheKeys) FindCacheKeyByAnyGet(target *models.TargetStruct) bool {
 	return false
 }
 
-func (f *FindCacheKeys) BinarySearchHeaders(target *models.TargetStruct) (bool, []string) {
-	var wg sync.WaitGroup
-	var headerValues sync.Map
-	var search func(target *models.TargetStruct, headers []string)
-
-	search = func(target *models.TargetStruct, headers []string) {
-		defer wg.Done()
-		if len(headers) == 0 {
-			return
-		}
-		mid := len(headers) / 2
-		leftPart := headers[:mid]
-		rightPart := headers[mid:]
-
-		tryRequest := func(headers []string) (*http.Response, error) {
-			tmpReq, _ := utils.CloneRequest(target.Request)
-			hvMap := make(map[string]string, len(headers))
-			for _, h := range headers {
-				hvMap[h] = utils.RandomString(5)
-			}
-			return f.GetRespByDefHeader(tmpReq, hvMap, target.Response.StatusCode)
-		}
-
-		var resp *http.Response
-		var err error
-		resp, err = tryRequest(headers)
-		// 异常状态码 未命中缓存不能直接丢弃，只有当发起请求错误，或者状态码正常且命中缓存的请求丢弃
-		if err != nil || (resp.StatusCode == target.Response.StatusCode && utils.IsCacheHit(target, &resp.Header)) {
-			return
-		}
-		utils.CloseReader(resp.Body)
-		if len(headers) == 1 {
-			if utils.IsCacheMiss(target, &resp.Header) {
-				resp2, err := utils.CommonClient.Do(resp.Request)
-				if err != nil {
-					return
-				}
-				if utils.IsCacheHit(target, &resp2.Header) {
-					headerValues.Store(headers[0], utils.RandomString(5))
-				}
-			}
-			return
-		}
-		wg.Add(2)
-		go search(target, leftPart)
-		go search(target, rightPart)
-	}
-
-	wg.Add(1)
-	search(target, f.HeaderWordList)
-	wg.Wait()
-
-	var cacheKeyList []string
-	headerValues.Range(func(key, value interface{}) bool {
-		cacheKeyList = append(cacheKeyList, key.(string))
-		return true
-	})
-
-	return len(cacheKeyList) != 0, cacheKeyList
-}
-
-//func (f *FindCacheKeys) BinarySearchHeaders2(target *models.TargetStruct) (bool, []string) {
-//	// 使用WaitGroup来等待所有goroutines完成
+//func (f *FindCacheKeys) BinarySearchHeaders(target *models.TargetStruct) (bool, []string) {
 //	var wg sync.WaitGroup
-//	// 缓存键
 //	var headerValues sync.Map
-//	// 闭包函数，用于递归调用
 //	var search func(target *models.TargetStruct, headers []string)
+//
 //	search = func(target *models.TargetStruct, headers []string) {
-//		// 每次递归调用结束时减少计数
 //		defer wg.Done()
-//		// 递归结束条件
 //		if len(headers) == 0 {
 //			return
 //		}
 //		mid := len(headers) / 2
 //		leftPart := headers[:mid]
 //		rightPart := headers[mid:]
-//		// 克隆请求和获取响应的逻辑放在递归调用之前，以避免重复
-//		tmpReq, _ := utils.CloneRequest(target.Request)
-//		hvMap := make(map[string]string, len(headers))
-//		for _, h := range headers {
-//			hvMap[h] = utils.RandomString(5)
-//		}
-//		resp, err := f.GetRespByDefHeader(tmpReq, hvMap, target.Response.StatusCode)
-//		// 检查响应，如果resp为nil，则直接返回，避免关闭空的Body
-//		if resp != nil {
-//			// 使用defer确保关闭
-//			defer utils.CloseReader(resp.Body)
-//			// 如果resp不为nil且hvMap的长度为1,重复判断下是否未命中缓存，防止目标站点存在极短缓存时间的状况
-//			if len(hvMap) == 1 {
-//				// 异常状态码忽略，例如If-Match等
-//				if resp.StatusCode != target.Response.StatusCode {
-//					return
-//				}
-//				// 如果未命中缓存
-//				if utils.IsCacheMiss(target, &resp.Header) {
-//					// 线程安全
-//					for h, v := range hvMap {
-//						headerValues.Store(h, v)
-//					}
-//				}
-//				// 命中缓存或者未命中缓存，结束递归
-//				return
+//
+//		tryRequest := func(headers []string) (*http.Response, error) {
+//			tmpReq, err := utils.CloneRequest(target.Request)
+//			if err != nil {
+//				return nil, err
 //			}
+//			hvMap := make(map[string]string, len(headers))
+//			for _, h := range headers {
+//				hvMap[h] = utils.RandomString(5)
+//			}
+//			return f.GetRespByDefHeader(tmpReq, hvMap, target.Response.StatusCode)
 //		}
 //
-//		// 进行递归调用
-//		if err != nil || (resp != nil && utils.IsCacheMiss(target, &resp.Header)) {
-//			wg.Add(2)                    // 增加WaitGroup的计数
-//			go search(target, leftPart)  // 递归搜索左半部分
-//			go search(target, rightPart) // 递归搜索右半部分
+//		var resp *http.Response
+//		var err error
+//		resp, err = tryRequest(headers)
+//		// 异常状态码 未命中缓存不能直接丢弃，只有当发起请求错误，或者状态码正常且命中缓存的请求丢弃
+//		if err != nil || (resp.StatusCode == target.Response.StatusCode && utils.IsCacheHit(target, &resp.Header)) {
+//			return
 //		}
+//		utils.CloseReader(resp.Body)
+//		if len(headers) == 1 {
+//			if utils.IsCacheMiss(target, &resp.Header) {
+//				resp2, err := utils.CommonClient.Do(resp.Request)
+//				if err != nil {
+//					return
+//				}
+//				if utils.IsCacheHit(target, &resp2.Header) {
+//					headerValues.Store(headers[0], utils.RandomString(5))
+//				}
+//			}
+//			return
+//		}
+//		wg.Add(2)
+//		go search(target, leftPart)
+//		go search(target, rightPart)
 //	}
-//	// 初始化WaitGroup计数
+//
 //	wg.Add(1)
 //	search(target, f.HeaderWordList)
 //	wg.Wait()
-//	cacheKeyList := make([]string, 0)
-//	headerValues.Range(
-//		func(key, value interface{}) bool {
-//			cacheKeyList = append(cacheKeyList, key.(string))
-//			return true
-//		})
-//	if len(cacheKeyList) != 0 {
-//		return true, cacheKeyList
-//	}
-//	return false, cacheKeyList
+//
+//	var cacheKeyList []string
+//	headerValues.Range(func(key, value interface{}) bool {
+//		cacheKeyList = append(cacheKeyList, key.(string))
+//		return true
+//	})
+//	return len(cacheKeyList) != 0, cacheKeyList
 //}
 
-// GetRespByDefHeader 随机 Header 参数和随机 Value 获取一个响应
-func (f *FindCacheKeys) GetRespByDefHeader(req *http.Request, hvMap map[string]string, primitiveRespStatusCOde int) (*http.Response, error) {
-	if len(hvMap) == 0 {
-		err := fmt.Errorf("hvMap 为空")
-		return nil, err
+// FindCacheKeyByHeader 寻找请求头缓存键
+func (f *FindCacheKeys) FindCacheKeyByHeader(target *models.TargetStruct) bool {
+	var wg sync.WaitGroup
+	headers := models.Config.Headers
+	wg.Add(1)
+	go f.BinarySearchHeaders(target, headers, &wg)
+	wg.Wait()
+	if target.Cache.CKIsHeader {
+		return true
 	}
-	for k, v := range hvMap {
-		req.Header.Set(k, v)
-	}
-	return utils.CommonClient.Do(req)
-	//resp, err := utils.CommonClient.Do(req)
-	//if err != nil {
-	//	return nil, err
-	//}
-	// 因为有些服务接受不了很长的请求头，会响应一个异常状态码，如果是这种情况的话就响应一个错误，上层调用方需要减小请求头的数量
-	//if resp.StatusCode != primitiveRespStatusCOde {
-	//	err = fmt.Errorf("the target %s returned an unexpected status code: %d", req.URL, resp.StatusCode)
-	//	return resp, err
-	//}
-	//return resp, err
+	return false
 }
+
+// BinarySearchHeaders 二分法搜索 Header 缓存键
+func (f *FindCacheKeys) BinarySearchHeaders(target *models.TargetStruct, params []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	tmpRequest, err := utils.CloneRequest(target.Request)
+	if err != nil {
+		gologger.Error().Msg(err.Error())
+		return
+	}
+	mid := len(params) / 2
+	leftPart := params[:mid]
+	rightPart := params[mid:]
+
+	for _, h := range params {
+		tmpRequest.Header.Add(h, utils.RandomString(5))
+	}
+	tmpResp, err := utils.CommonClient.Do(tmpRequest)
+	if err != nil {
+		wg.Add(2)
+		go f.BinarySearchHeaders(target, leftPart, wg)
+		go f.BinarySearchHeaders(target, rightPart, wg)
+		return
+	}
+	utils.CloseReader(tmpResp.Body)
+	if tmpResp.StatusCode == target.Response.StatusCode && utils.IsCacheHit(target, &tmpResp.Header) {
+		return
+	}
+	if len(params) == 1 {
+		if utils.IsCacheMiss(target, &tmpResp.Header) {
+			target.Cache.CKIsHeader = true
+			target.Cache.HeaderCacheKeys = append(target.Cache.HeaderCacheKeys, params[0])
+		}
+		return
+	}
+	wg.Add(2)
+	go f.BinarySearchHeaders(target, leftPart, wg)
+	go f.BinarySearchHeaders(target, rightPart, wg)
+}
+
+// GetRespByDefHeader 随机 Header 参数和随机 Value 获取一个响应
+//func (f *FindCacheKeys) GetRespByDefHeader(req *http.Request, hvMap map[string]string, primitiveRespStatusCOde int) (*http.Response, error) {
+//	if len(hvMap) == 0 {
+//		err := fmt.Errorf("hvMap 为空")
+//		return nil, err
+//	}
+//	for k, v := range hvMap {
+//		req.Header.Set(k, v)
+//	}
+//	return utils.CommonClient.Do(req)
+//}
 
 // GetRespByDefGetParams 随机 GET 参数和随机 Value 获取一个响应
 func (f *FindCacheKeys) GetRespByDefGetParams(req *http.Request, key, value string) (resp *http.Response, err error) {
@@ -393,9 +364,11 @@ func (f *FindCacheKeys) BinarySearchGetCacheKey(target *models.TargetStruct, par
 	if tmpResp.StatusCode == target.Response.StatusCode && utils.IsCacheHit(target, &tmpResp.Header) {
 		return
 	}
-	if len(params) == 1 && utils.IsCacheMiss(target, &tmpResp.Header) {
-		target.Cache.CKIsGet = true
-		target.Cache.GetCacheKeys = append(target.Cache.GetCacheKeys, params[0])
+	if len(params) == 1 {
+		if utils.IsCacheMiss(target, &tmpResp.Header) {
+			target.Cache.CKIsGet = true
+			target.Cache.GetCacheKeys = append(target.Cache.GetCacheKeys, params[0])
+		}
 		return
 	}
 	wg.Add(2)
